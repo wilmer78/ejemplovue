e025d38f66bfd54322f49f7cc3ab6738b9370a9c
wilmer78 <wilmer78@gmail.com>
2020-05-21 12:54:25 -0430

    procesar

---
+++ src/components/Lista.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ul class="list-group">
+      <li v-for="(ide,index) of ordena" @click="suma(index)" :key="ide.id"
+      class="list-group-item d-flex justify-content-between align-items-center">
+        {{index}}   {{ide.nombre}}
+        <span class="badge badge-primary badge-pill">{{ide.voto}}</span>
+      </li>
+    </ul>
+     <button @click="cero" class="btn btn-danger" >Cero</button>
+  </div>
+</template>
+
+<script>
+import {mapState,mapMutations} from 'vuex';
+export default {
+  name: 'Lista',
+  computed:{
+    ...mapState(['ides']),
+    ordena(){
+      return this.ides.sort( (a,b)=> b.voto - a.voto )
+    }
+    },
+  methods:{
+    ...mapMutations(['suma','cero'])
+  }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    ides:[
+      {nombre:'vs code',voto:0},
+      {nombre:'netbeans',voto:0},
+      {nombre:'sublime',voto:0}
+    ]
+  },
+  mutations: {
+    suma(state,index){
+      state.ides[index].voto++;
+    },
+    cero(state){
+      state.ides.forEach(elemento=>{
+        elemento.voto=0;
+      })
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
