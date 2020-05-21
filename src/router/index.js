98dd9a975e988665c59eda759f5832f9835527c7
wilmer78 <wilmer78@gmail.com>
2020-05-21 09:03:21 -0430

    Version de prueba

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/caracteristicas/:id',
+    name: 'Caracteristicas',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "caracteristicas" */ '../views/Caracteristicas.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Caracteristicas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="caracteristicas">
+    <h1>caracteristicas desde vista</h1>
+    <router-link class="btn btn-success m-1"
+    v-for="(car,index) of listaCar"
+    :to="{name: 'Caracteristicas',params:{id:car}}" :key="index"
+    > {{car}} </router-link>
+    <ComCaracteristicas/>
+    <button @click="home" >Home</button>
+    <button @click="atras" >Atras</button>
+    <button @click="adelante" >Adelante</button>
+  </div>
+</template>
+
+<script>
+import ComCaracteristicas from "../components/ComCaracteristicas.vue"
+export default {
+  components:{
+    ComCaracteristicas
+  },
+  data(){
+    return{
+      listaCar:[1,2,3]
+    }
+  },
+  methods:{
+    home(){
+      this.$router.push("/")
+    },
+    atras(){
+      this.$router.go(-1)
+    },
+    adelante(){
+      this.$router.go(1)
+    },
+  }
+}
+</script>
